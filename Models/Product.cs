64d9574317898a2ec408ec82d671f9a7db72b15b
Michal Spocinski <s20461@pjwstk.edu.pl>
2021-04-11 15:22:57 +0200

    Finished creating Product.cs model.

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,12 +1,21 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Zadanie_5.Models
 {
     public class Product
     {
+        [Required(ErrorMessage = "Product Id is required", AllowEmptyStrings = false)]
         public int IdProduct { get; set; }
+        
+        [Required(ErrorMessage = "Warehouse Id is required", AllowEmptyStrings = false)]
         public int IdWarehouse { get; set; }
+        
+        [Required(ErrorMessage = "Amount is required", AllowEmptyStrings = false)]
+        [Range(1,int.MaxValue)]
         public int Amount { get; set; }
+        
+        [Required(ErrorMessage = "Timestamp is required", AllowEmptyStrings = false)]
         public DateTime CreatedAt { get; set; }
 
         public override string ToString()
